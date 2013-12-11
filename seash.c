18a5c0eed33540ad2460b1a59025540071f328ea
almir <almir@dzinovic.net>
2013-12-11 21:05:19 +0100

    Get user's login shell and open that one instead of always opening bash.

--- seash.c
+++ seash.c
@@ -18,11 +18,29 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main (void)
-{
+int main (void) {
+   // Print welcome message and Barney's quote
    printf ("\x1b[36mWelcome to \x1b[31mSea Shell\x1b[36m! The most awesome shell on Earth.\x1b[0m\n");
-   printf("\x1b[33m\"Everytime I get sad, I stop beeing sad and be awesome instead. True story.\" \x1b[31m(\x1b[32mBarney Stinson\x1b[31m)\x1b[0m\n");
-   system ("bash");
+   printf ("\x1b[33m\"Everytime I get sad, I stop beeing sad and be awesome instead. True story.\" \x1b[31m(\x1b[32mBarney Stinson\x1b[31m)\x1b[0m\n");
+   
+   // Get user's login shell
+   char *shell = getenv ("SHELL");
+   
+   // Check value of the user's login shell and in case it hasn't been set,
+   // or if it's value is /usr/bin/seash, set shell variable to /bin/bash if
+   // it exists, otherwise just set it to /bin/sh
+   if (shell == NULL || strcmp (shell, "/usr/bin/seash") == 0) {
+      if (access ("/bin/bash", 0) != -1) {
+         strcpy(shell, "/bin/bash");
+      } else {
+         strcpy(shell, "/bin/sh");
+      }
+   }
+   
+   // Open shell
+   system (shell);
+   
    return 0;
 }
